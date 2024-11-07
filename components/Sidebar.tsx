4882951f92d41b5a5edabb14a602a932a2455ce1
ThanhTris <thanhtritran8@gmail.com>
2024-11-07 22:30:29 +0700

    update sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from "lucide-react";
+import Logo from "./Logo";
 
 const routes = [
   {
@@ -27,7 +28,11 @@ const routes = [
 ];
 
 const DesktopSidebar = () => {
-  return <div>DesktopSidebar</div>;
+  return <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
+    <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+        <Logo />
+    </div>
+  </div>;
 };
 
 export default DesktopSidebar;
