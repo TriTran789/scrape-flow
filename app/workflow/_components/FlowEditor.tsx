029d99d1a56b879475e05b31a73ffdb091101fb8
ThanhTris <thanhtritran8@gmail.com>
2024-11-09 07:25:38 +0700

    add node to react-flow

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Workflow } from "@prisma/client";
 import {
-    Background,
+  Background,
   BackgroundVariant,
   Controls,
   ReactFlow,
@@ -11,9 +11,18 @@ import {
 import React from "react";
 
 import "@xyflow/react/dist/style.css";
+import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
+import { TaskType } from "@/types/task";
+import NodeComponent from "./nodes/NodeComponent";
+
+const nodeTypes = {
+  FlowScrapeNode: NodeComponent,
+};
 
 const FlowEditor = ({ workflow }: { workflow: Workflow }) => {
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState([
+    CreateFlowNode(TaskType.LAUNCH_BROWSER),
+  ]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   return (
     <main className="h-full w-full">
@@ -22,6 +31,7 @@ const FlowEditor = ({ workflow }: { workflow: Workflow }) => {
         edges={edges}
         onEdgesChange={onEdgesChange}
         onNodesChange={onNodesChange}
+        nodeTypes={nodeTypes}
       >
         <Controls position="top-left" />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
