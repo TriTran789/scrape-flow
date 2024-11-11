28898cc5c83e8157a2175a48c136951420f7c14f
ThanhTris <thanhtritran8@gmail.com>
2024-11-11 10:18:27 +0700

    update stringparam

--- app/workflow/_components/nodes/NodeParamField.tsx
+++ app/workflow/_components/nodes/NodeParamField.tsx
@@ -1,12 +1,43 @@
 "use client";
-import React from "react";
+import React, { useCallback } from "react";
 import { TaskParam, TaskParamType } from "@/types/task";
 import StringParam from "./param/StringParam";
+import { useReactFlow } from "@xyflow/react";
+import { AppNode } from "@/types/appNode";
 
-const NodeParamField = ({ param }: { param: TaskParam }) => {
+const NodeParamField = ({
+  param,
+  nodeId,
+}: {
+  param: TaskParam;
+  nodeId: string;
+}) => {
+  const { updateNodeData, getNode } = useReactFlow();
+  const node = getNode(nodeId) as AppNode;
+  const value = node?.data.inputs?.[param.name];
+  console.log("@VALUE", value);
+
+  const updateNodeParamValue = useCallback(
+    (newValue: string) => {
+      updateNodeData(nodeId, {
+        inputs: {
+          ...node?.data.inputs,
+          [param.name]: newValue,
+        },
+      });
+    },
+    [nodeId, updateNodeData, param.name, node?.data.inputs]
+  );
+
   switch (param.type) {
     case TaskParamType.STRING:
-      return <StringParam />;
+      return (
+        <StringParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+        />
+      );
     default:
       return (
         <div className="w-full">
--- app/workflow/_components/nodes/param/StringParam.tsx
+++ app/workflow/_components/nodes/param/StringParam.tsx
@@ -1,8 +1,29 @@
-"use client"
-import React from "react";
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ParamProps } from "@/types/appNode";
+import { TaskParam } from "@/types/task";
+import React, { useId } from "react";
 
-const StringParam = () => {
-  return <div>StringParam</div>;
+const StringParam = ({ param, value, updateNodeParamValue }: ParamProps) => {
+  const id = useId();
+  return (
+    <div className="space-y-1 p-1 w-full">
+      <Label htmlFor={id} className="text-xs flex">
+        {param.name}
+        {param.required && <p className="text-red-400 px-2">*</p>}
+      </Label>
+      <Input
+        id={id}
+        defaultValue={value}
+        placeholder="Enter value here"
+        onChange={(e) => updateNodeParamValue(e.target.value)}
+      />
+      {param.helperText && (
+        <p className="text-muted-foreground px-2">{param.helperText}</p>
+      )}
+    </div>
+  );
 };
 
 export default StringParam;
